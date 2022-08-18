2822a4a32033d5fb534eb05212d2cb3dfed4aa15
hidayat <dayatznur@gmail.com>
2022-08-18 13:12:40 +0700

    fix head information

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -4,21 +4,25 @@ import { IMG } from "../assets/images";
 function Home() {
   return (
     <div>
-      <div>
+      <div className="grid grid-cols-2 h-96 items-center px-56">
         <div>
-          <p>
-            Hi, M N Hidayat,
-            <br /> React Native Engineer
+          <p className="text-gray-800 text-3xl font-medium mb-2">
+            Hi, I am M N Hidayat,
+            <br /> React Native Engineer from Indonesia.
           </p>
-          <p>
+          <p className="text-gray-400 text-xl">
             I have experience in react native developer for 3 years, previously
             I was also a web developer with the technology used html, css,
             jquery & codeigniter. Hope to work with you in the future
           </p>
-          <button onClick={() => null}>Download Resume</button>
         </div>
+
         <div>
-          <img src={IMG.AVATAR} alt="Hidayat" />
+          <img
+            alt="profil"
+            src={IMG.AVATAR}
+            className="mx-auto object-cover rounded-full h-40 w-40"
+          />
         </div>
       </div>
     </div>
