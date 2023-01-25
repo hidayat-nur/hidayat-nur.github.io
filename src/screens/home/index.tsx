12ed91d97f2e40a0426c7b863c79aed37fd4a73e
hidayat <dayatznur@gmail.com>
2023-01-25 09:10:08 +0700

    update page with helping by chat gpt

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,50 +1,51 @@
 import React from "react";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import { IMG } from "../../assets/images";
 import Files from "./../../assets/files";
 import "./styles.css";
 
 function Home() {
   return (
-    <div className="global-container">
-      <Container>
-        <Row>
-          <Col xs={12} md={6} lg={8} className="col_bt">
-            <div className="wrap-info">
-              <h2 className="introLabel">Hi there 👋 I`m Hidayat</h2>
-              <p className="introSubLabel">
-                🔭 I’m currently working on{" "}
-                <a
-                  href="https://www.moduit.id/"
-                  target="_blank"
-                  className="link"
-                  rel="noreferrer"
-                >
-                  moduit.id
-                </a>{" "}
-                as a react native developer. Working with lots of adding new
-                features and bugfix issues to make the app even better.
-                <br />
-                🌱 Outside of work, I learned a lot of correct coding
-                fundamentals such as clean code, code optimization, and other
-                basic things that I think are important in building applications
-                in a cool and correct way.
-                <br />
-                <br />
-                Warm regards from me 😄
-              </p>
-              <Button className="btnCV" href={Files.cv} target="_blank">
-                Download Resume
-              </Button>
-            </div>
-          </Col>
-          <Col xs={12} md={6} lg={4} className="col_bt right_col_desktop">
-            <div className="wrapAvatar">
-              <Image src={IMG.SALY} className="avatar" roundedCircle />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <div className="center-section">
+      <div className="portfolio-page">
+        <div className="header-section">
+          <img
+            src={IMG.SALY}
+            alt="Profile Picture"
+            className="profile-picture"
+          />
+          <div>
+            <h1 className="light-text">Hi there 👋 I`m Hidayat</h1>
+            <p className="light-text">Software engineer in mobile</p>
+          </div>
+        </div>
+        <div className="main-section">
+          <h2 className="light-text">About Me</h2>
+          <p className="light-text">
+            🔭 I’m currently working on{" "}
+            <a
+              href="https://www.moduit.id/"
+              target="_blank"
+              className="link"
+              rel="noreferrer"
+            >
+              moduit.id
+            </a>{" "}
+            as a react native developer. Working with lots of adding new
+            features and bugfix issues to make the app even better.
+            <br />
+            🌱 Outside of work, I learned a lot of correct coding fundamentals
+            such as clean code, code optimization, and other basic things that I
+            think are important in building applications in a cool and correct
+            way.
+            <br />
+            <br />
+            Warm regards from me 😄
+          </p>
+          <a href={Files.cv} download className="download">
+            Download Resume
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
