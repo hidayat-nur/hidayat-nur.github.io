b43dd109500f4b64f7aa4423cb4672cc59f6e7b7
hidayat <dayatznur@gmail.com>
2023-04-08 10:05:45 +0700

    Refactor home screen with modular components for better code organization.
    - Adds an intro section to the home screen, with an avatar, title, and description.
    - Modularizes the header section by creating new components for better maintainability and removes social section from the header.
    - Adds a blog list and footer component to the home screen.
    - Introduces `HeaderComponent` that includes a header layout with links to social platforms and site navigation.
    - Renames the CSS class selector for the footer component.
    - Adds a new component for a list of latest articles with each list item having an anchor element and a publication date.

---
+++ src/components/footer-component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const FooterComponent = () => {
+  return (
+    <footer>
+      <p className="copyright">
+        © 2023 <b>M Nur Hidayat</b>. All Rights Reserved.
+      </p>
+      <p className="tech">
+        Powered by: React, Typescript, Eslint, Github And Etc.
+      </p>
+    </footer>
+  );
+};
---
+++ src/components/header-component.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { IMG } from "../assets/images";
+
+export const HeaderComponent = () => {
+  return (
+    <header>
+      <a href="/">
+        <img
+          src={IMG.HIDAYATLAB_LOGO}
+          className="logo"
+          alt="Hidayat Nur Lab Logo"
+        />
+      </a>
+
+      <div className="social">
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/hidayat-nur-51480210a/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={IMG.LINKEDIN_LOGO} alt="LinkedIn logo" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/hidayat-nur"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={IMG.GITHUB_LOGO} alt="GitHub logo" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@border-radius"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={IMG.YOUTUBE_LOGO} alt="YouTube logo" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <nav className="navigation">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+          <li>
+            <a href="#">Experiences</a>
+          </li>
+          <li>
+            <a href="#">About me</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
